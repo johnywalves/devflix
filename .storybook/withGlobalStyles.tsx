6c354869c3a825b565b529bfd5bfc5dda41cae08
Johny W. Alves <johnywalves@gmail.com>
2020-07-28 18:36:43 -0300

    Initial commit from Create Next App

---
+++ .storybook/withGlobalStyles.tsx
@@ -0,0 +1,12 @@
+import { StoryFn } from '@storybook/addons'
+
+import GlobalStyles from '../src/styles/global'
+
+const withGlobalStyles = (storyFn: StoryFn) => (
+  <>
+    <GlobalStyles />
+    {storyFn()}
+  </>
+)
+
+export default withGlobalStyles
---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/*stories.tsx'],
+  setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts']
+}
